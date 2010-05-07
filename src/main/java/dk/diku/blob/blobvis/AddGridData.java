7c248640fc0e76979fd8d8dcf9fc6026297a14fb
Søren Bjerregaard Vrist <sbv@T60XPSBV.nclan.netcompany.dk>
2010-05-07 11:29:15 +0200

    More

---
+++ src/main/java/dk/diku/blob/blobvis/AddGridData.java
@@ -0,0 +1,13 @@
+package dk.diku.blob.blobvis;
+
+import model.BondSite;
+
+public class AddGridData {
+	public int countNodes =0;
+	public int cardinality = 0;
+	public boolean randomData = true;
+	public boolean randomBs = true;
+	public boolean randomCardinality = true;
+	public BondSite out;
+	public BondSite in;
+}
